8c4b37177361a1c422c16b6e27fcc75ca16f8f48
mirae9126 <mirae91@outlook.com>
2023-02-27 21:41:27 +0900

    second commit

---
+++ 구구단2._1py.py
@@ -0,0 +1,7 @@
+print("9단 구구단 프로젝트, 챗GPT3.5_2022.11.30", 230227)
+def multiplication_table(n):
+    for i in range(1, 10):
+        print(n, 'x', i, '=', n*i)
+
+multiplication_table(9)
+print("99단 프로그램, 깃허브 git push...\n 2023.2.27 대상 옛터")
